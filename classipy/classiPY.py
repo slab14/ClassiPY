22d8855bc1e7070b91b324cd09be454873ea2f6f
slab14 <mmccorm1@andrew.cmu.edu>
2024-02-12 19:37:25 -0600

    initial version of banners. text is incorrect

--- classipy/classiPY.py
+++ classipy/classiPY.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 """This module provides an interface for labelling all images in a folder"""
 import click
+import logging
 from pathlib import Path
-#from PIL import Image, ImageDraw, ImageFont
-from wand.image import Image
-from wand.color import Color
-from wand.drawing import Drawing
+from PIL import Image, ImageDraw, ImageFont
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 BANNER_PERCENT_OF_HEIGHT = 5  # Adjust the percentage as needed
 FONT_PERCENT_OF_BANNER = 50  # Adjust the font size percentage as needed
+FONT_SIZE = 25
+BAR_HEIGHT = 40
 EXTS = (".jpg", ".png", ".jpeg", ".bmp")  # file extensions to look for
 MARKS = ("CUI", "S", "U")
+FONT = Path(__file__).parent / ".." / "font" / "ARIALBD.TTF"
 
 
 class Markings:
@@ -38,23 +44,106 @@ class Markings:
         out = None
         if classification == "CUI":
             out = (80, 43, 133)
-            out = '#502b85'
         elif classification == "S":
             out = (200, 16, 46)
-            out = 'red'
         elif classification == "U":
             out = (0, 122, 51)
-            out = 'green'
         return out
 
 
 markings = Markings()
 
 
+def draw_overlay(
+        draw: ImageDraw.ImageDraw, width: int, height: int,
+        classification: str='CUI'
+) -> None:
+    #font = ImageFont.truetype(str(FONT), FONT_SIZE)
+    #banner_height = int(height * (BANNER_PERCENT_OF_HEIGHT / 100))
+    banner_height = BAR_HEIGHT
+    draw.rectangle(
+        [0, 0, width, banner_height],
+        fill=markings.get_color(classification)
+    )
+    draw.rectangle(
+        [0, height-banner_height, width, height],
+        fill=markings.get_color(classification)
+    )
+    text_bbox = draw.textbbox(
+        (0, 0), markings.get_long_name(classification)
+    )
+    text_horizontal = width - (text_bbox[2] - text_bbox[0]) // 2
+    text_vertical = banner_height - (text_bbox[3] + text_bbox[1]) // 2
+    text_position_top = (text_horizontal, text_vertical)
+    text_position_bottom = (
+        text_horizontal,
+        height - banner_height + text_vertical
+    )
+    draw.text(
+        text_position_top, markings.get_long_name(classification),
+        fill=(255, 255, 255)
+    )
+    draw.text(
+        text_position_bottom, markings.get_long_name(classification),
+        fill=(255, 255, 255)
+    )    
+
+
+def add_border(img: Image.Image, border_thickness: int=5) -> Image.Image:
+    width, height = img.size
+    bordered_img = Image.new(
+        "RGB",
+        (width + 2 * border_thickness, height + 2 * border_thickness),
+        (0, 0, 0)
+    )
+    bordered_img.paste(img, (border_thickness, border_thickness))
+    return bordered_img
+    
+
+def save_image_with_overlay(
+        image_path: Path, new_path: Path,
+        classification: str, border_thickness: int=5
+) -> None:
+    try:
+        img = Image.open(image_path)
+        width, height = img.size
+        banner_height = int(height * (BANNER_PERCENT_OF_HEIGHT / 100))    
+        new_height = height + 2 * banner_height
+        new_img = Image.new("RGB", (width, new_height), (0, 0, 0))
+        draw = ImageDraw.Draw(new_img)
+        draw_overlay(draw, width, new_height, classification)
+        new_img.paste(img, (0, banner_height))
+        bordered_img = add_border(new_img, border_thickness)
+        bordered_img.save(new_path)
+        logging.info(f"Saved {image_path.name} to: {new_path}")
+    except FileNotFoundError:
+        logging.error(f"Error: File not found at {image_path}")
+    except Exception as e:
+        logging.error(f"Error opening image: {e}")
+    
+
+def add_overlay_to_dir(
+        directory_path: Path, output_path: Path,
+        classification: str='CUI') -> None:
+    if not Path(output_path).exists():
+        output_path.mkdir(parents=True, exist_ok=True)
+    image_files = [
+        x for x in Path(directory_path).iterdir() if x.suffix.lower() in EXTS
+    ]
+    for image_file in image_files:
+        overlayed_image = Path(
+            output_path,
+            f"({markings.get_symbol(classification)}) {image_file.name}"
+        )
+        save_image_with_overlay(
+            image_file, overlayed_image, classification
+        )
+
+
 def add_banner(input_image, output_image, classification="CUI"):
     """Function to add top & bottom portion markings to image file"""
     with Image(filename=input_image) as img:
-        banner_height = int(img.height * (BANNER_PERCENT_OF_HEIGHT / 100))
+
         banner = Image(width=img.width, height=banner_height)
         banner.background_color = markings.get_color(classification)
         banner.alpha_channel = "remove"
@@ -129,8 +218,8 @@ def label_folder(input_folder, output_folder, classification="CUI"):
 def main(images, classification, output):
     images = Path(images).resolve()
     output = images if output is None else Path(output).resolve()
-
-    label_folder(images, output, classification)
+    #label_folder(images, output, classification)
+    add_overlay_to_dir(images, output, classification)
 
 
 if __name__ == "__main__":
