185ec510dbc266f8860a2ff1a8a45ad50a4ec0c7
slab14 <mmccorm1@andrew.cmu.edu>
2024-02-15 06:36:43 -0600

    removing unused code

--- classipy/classiPY.py
+++ classipy/classiPY.py
@@ -130,67 +130,6 @@ def add_overlay_to_dir(
         )
 
 
-def add_banner(input_image, output_image, classification="CUI"):
-    """Function to add top & bottom portion markings to image file"""
-    with Image(filename=input_image) as img:
-
-        banner = Image(width=img.width, height=banner_height)
-        banner.background_color = markings.get_color(classification)
-        banner.alpha_channel = "remove"
-
-        # Calculate font size based on a percentage of the banner's height
-        font_size = int(banner_height * (FONT_PERCENT_OF_BANNER / 90))
-
-        with Drawing() as draw:
-            draw.font_size = font_size
-            draw.font_weight = 700  # Bold font weight
-            draw.fill_color = Color("white")
-            draw.text_alignment = "center"
-            draw.text(
-                x=banner.width // 2,
-                y=banner_height // 2 + font_size // 3,
-                body=markings.get_long_name(classification),
-            )
-            draw.text(
-                x=banner.width // 2,
-                y=img.height + 2 * banner_height // 2 + font_size // 3,
-                body=markings.get_long_name(classification),
-            )
-            draw(banner)
-
-        with Image(
-            filename=input_image
-        ) as img:  # Re-open the image to avoid closure issue
-            with Image(
-                width=img.width, height=img.height + 2 * banner_height
-            ) as result:
-                result.composite(banner, top=0, left=0)
-                result.composite(img, top=banner_height, left=0)
-                result.composite(banner, top=img.height + banner_height, left=0)
-                border_style = 6  # Change border
-                result.border(
-                    color=Color("black"), width=border_style,
-                    height=border_style
-                )
-                result.save(filename=output_image)
-
-
-def label_folder(input_folder, output_folder, classification="CUI"):
-    """Function to rename all files with classification marking \
-    Calls the `add_banner` function to mark images """
-    if not Path(output_folder).exists():
-        output_folder.mkdir(parents=True, exist_ok=True)
-
-    image_files = [
-        x for x in Path(input_folder).iterdir() if x.suffix.lower() in EXTS
-    ]
-
-    for image_file in image_files:
-        output_file = Path(
-            output_folder, f"({markings.get_symbol(classification)}) {image_file.name}"
-        )
-        add_banner(image_file, output_file, classification)
-
 @click.command()
 @click.argument(
     'images', required=True,
